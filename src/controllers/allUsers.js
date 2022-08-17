625dc53942525e303a70eed217f4c7a02d624365
unknown <saritadh74@gmail.com>
2022-08-17 11:41:06 +0300

    added the catch relates #9

--- src/controllers/allUsers.js
+++ src/controllers/allUsers.js
@@ -2,16 +2,18 @@ const path = require('path');
 const fetchData = require('./fetch');
 
 const allUsersRoute = (req, res) => {
-  fetchData('https://api.github.com/users').then((data) => {
-    let requests = [];
-    data.forEach((e, i) => {
-      requests.push(fetchData(e.url));
-    });
+  fetchData('https://api.github.com/users')
+    .then((data) => {
+      let requests = [];
+      data.forEach((e, i) => {
+        requests.push(fetchData(e.url));
+      });
 
-    Promise.all(requests).then((data) => {
-      res.send(data);
-    });
-  });
+      Promise.all(requests).then((data) => {
+        res.send(data);
+      });
+    })
+    .catch((err) => console.log('error in fetching all users'));
 };
 
 module.exports = allUsersRoute;

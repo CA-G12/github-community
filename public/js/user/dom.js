c8134b740bb2335ce1bbcf6e0909f5c554d0d5dc
mohammadalwadia@gmail.com <54046997+mwadia@users.noreply.github.com>
2022-08-17 14:28:29 +0300

    handled fetching repos dom realtes #26

--- public/js/user/dom.js
+++ public/js/user/dom.js
@@ -3,6 +3,32 @@ const profileName = document.querySelector('.user-about h2')
 const loginName = document.querySelector('.user-about h3')
 const bio = document.querySelector('.user-about p')
 const followers = document.querySelectorAll('.user-about span')
+const repoContainer = document.querySelector('.repo-cards')
+
+const handleRepos = (repos) => {
+    console.log(repos);
+    repoContainer.textContent = '';
+    repos.forEach(e => {
+        const card = document.createElement('div');
+        const repoDetails = document.createElement('div');
+        const repoLink = document.createElement('a');
+        const repoLanguages = document.createElement('p');
+        const repoDate = document.createElement('p');
+
+        card.classList.add('card');
+
+        repoLink.href = e.html_url;
+        repoLink.textContent = e.name;
+        repoLanguages.textContent = 'HTML, CSS, JS';
+        repoDate.textContent = `created at: ${e.created_at.split('T')[0]}`;
+
+        card.appendChild(repoDetails);
+        card.appendChild(repoDate);
+        repoDetails.appendChild(repoLink);
+        repoDetails.appendChild(repoLanguages);
+        repoContainer.appendChild(card);
+    })
+}
 
 const handleUser = (user) => {
     console.log(user);
@@ -13,3 +39,4 @@ const handleUser = (user) => {
     followers[0].textContent = user.followers;
     followers[1].textContent = user.following;
 }
+
--- public/js/user/fetch.js
+++ public/js/user/fetch.js
@@ -1,10 +1,18 @@
+const userName = window.location.search.split('=')[1];
 
 const getUser = () => {
-    const userName = window.location.search.split('=')[1];
 
     fetch(`/users/${userName}`)
         .then(data => data.json())
         .then(data => handleUser(data));
 }
 
-getUser()
+getUser();
+
+const getRepos = () => {
+    fetch(`/user/repos/${userName}`)
+        .then(data => data.json())
+        .then(data => handleRepos(data))
+}
+
+getRepos()

86077219179e39cf3f8ca6e0b19b251441ef3e91
unknown <saritadh74@gmail.com>
2022-08-17 04:10:58 +0300

    created the sever and set the environment up relates #2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
-// const express = require('express');
-// const compression = require('compression');
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+
+const app = express();
+app.disable('x-powered-by');
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+module.exports = app;
+
 // require('dotenv').config();
-//const apiKey = process.env.API_KEY;
+// const apiKey = process.env.API_KEY;
